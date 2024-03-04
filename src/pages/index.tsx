509b889f59c1a34157f37007a47d77917359c864
최아람 <aarmakrr@hanmail.net>
2024-03-05 00:30:35 +0900

    chore: react-query 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import * as stores from "@/data/store_data.json";
 import StoreBox from "@/components/StoreBox";
 import { StoreType } from "@/interface";
 
+import axios from "axios";
+
 export default function Home({ stores }: { stores: StoreType[] }) {
   const [map, setMap] = useState(null);
   const [currentStore, setCurrentStore] = useState(null);
@@ -19,12 +21,10 @@ export default function Home({ stores }: { stores: StoreType[] }) {
 }
 
 export async function getStaticProps() {
-  const stores = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/stores`
-  ).then((res) => res.json());
+  const stores = await axios(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`);
 
   return {
-    props: { stores },
+    props: { stores: stores.data },
     revalidate: 60 * 60,
   };
 }

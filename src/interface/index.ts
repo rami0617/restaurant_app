b492458f93ed531472d5b577f78995e7803d8f81
최아람 <aarmakrr@hanmail.net>
2024-03-04 23:09:04 +0900

    chore: 데이터베이스 연결

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,10 +1,19 @@
 export interface StoreType {
-  tel_no: string | null;
-  cob_code_nm: string;
-  bizcnd_code_nm: string;
-  upso_nm: string;
-  x_cnts: string;
-  y_dnts: string;
-  rdn_code_nm: string;
-  crtfc_gbn_nm: string;
+  id: number;
+  phone?: string | null;
+  address?: string | null;
+  lat?: string | null;
+  lng?: string | null;
+  name?: string | null;
+  category?: string | null;
+  storeType?: string | null;
+  foodCertifyName?: string | null;
+  // tel_no: string | null;
+  // cob_code_nm: string | null;
+  // bizcnd_code_nm: string | null;
+  // upso_nm: string | null;
+  // x_cnts: string | null;
+  // y_dnts: string | null;
+  // rdn_code_nm: string | null;
+  // crtfc_gbn_nm: string | null;
 }

55aa13a96dad6a793b2aabd669f91f4d3069f049
최아람 <aarmakrr@hanmail.net>
2024-03-04 23:11:42 +0900

    refactor: 안쓰는 코드 정리:

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -8,12 +8,4 @@ export interface StoreType {
   category?: string | null;
   storeType?: string | null;
   foodCertifyName?: string | null;
-  // tel_no: string | null;
-  // cob_code_nm: string | null;
-  // bizcnd_code_nm: string | null;
-  // upso_nm: string | null;
-  // x_cnts: string | null;
-  // y_dnts: string | null;
-  // rdn_code_nm: string | null;
-  // crtfc_gbn_nm: string | null;
 }
--- src/pages/api/stores.ts
+++ src/pages/api/stores.ts
@@ -2,20 +2,12 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { StoreType } from "@/interface";
 import { PrismaClient } from "@prisma/client";
 
-type Data = {
-  name: string;
-};
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<StoreType[]>
 ) {
   const prisma = new PrismaClient();
   const stores = await prisma.store.findMany();
-  //   res.status(200).json({ name: "John Doe" });
-  // const stores = (await import("../../data/store_data.json"))[
-  //   "DATA"
-  // ] as StoreType[];
 
   res.status(200).json(stores);
 }
